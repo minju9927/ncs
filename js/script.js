70d471868d351f502d22d76d9a2f0944862c4732
minju9927 <pink5214@naver.com>
2025-05-27 15:25:14 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(function () {
+
+
+    // fadeIn() /fadeOut()
+
+    $('.notice ul li:nth-child(1)').on('click', function () {
+        $('.modal-wrap ').fadeIn()
+
+    })
+    $('.closeBtn').on('click', function () {
+        $('.modal-wrap ').fadeOut()
+
+    })
+
+    // slideDown() , slideUp()
+    $('.nav-list').hover(function () {
+        $('.sub-list').stop().slideDown()
+    })
+
+    $('.sub-list').mouseleave(function () {
+
+        setTimeout(function () {//함수 내용을 몇초 뒤에 실행
+
+            $('.sub-list').stop().slideUp()
+        }, 1000)
+    })
+
+
+    // $('.go').on('click', function () {
+    //     window.location.href = $('#site').val()
+    //     console.log($('#site').val())
+    // })
+
+})
+
+
+document.querySelector('.go').addEventListener('click', function () {
+
+    console.log(document.querySelector('#site').value)
+
+
+    window.location.href = document.querySelector('#site').value;
+});
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+});
